f511c3a438137ceac1375fc2880f1fb60b70f568
Andrew <flysurea@gmail.com>
2017-08-11 15:01:21 -0400

    small edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(title)
+    if !title.empty?
+      full_title = title + " | App of the Century!"
+    else
+      full_title = "App of the Century!"
+    end
+  end
+
 end
